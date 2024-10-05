6d41c7647a984b53bfda881d02ffda6e834ea0e2
andres felipe <andresnarvaez304@gmail.com>
2024-10-05 09:52:45 -0500

    servidor mejorando

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require("express");
-const path = require("path");
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Obtener la ruta del archivo actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, "index.html")); // Asegúrate de que el archivo HTML esté en la raíz del proyecto
 });
 
 app.listen(PORT, () => {
